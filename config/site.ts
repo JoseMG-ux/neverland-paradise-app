dd3dbcb99200b3152f3fbaf3ae967c62e1acd70d
JoseMG-ux <joseutp14@gmail.com>
2025-06-27 18:19:00 -0500

    feat: Se agrego navegacion para tipo de actividades

--- config/site.ts
+++ config/site.ts
@@ -61,9 +61,28 @@ export const siteConfig = {
   ],
   links: {
     github: "https://github.com/heroui-inc/heroui",
-    twitter: "https://twitter.com/hero_ui",
-    docs: "https://heroui.com",
     discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
   },
+  navGroupDungeons : [
+    {
+      id: 1,
+      label: "Grupales",
+      href: "/activities/group-dungeons"
+    },
+    {
+      id: 2,
+      label: "Estáticas",
+      href: "/activities/static-dungeons"
+    },
+    {
+      id: 3,
+      label: "Title",
+      href: ""
+    },
+    {
+      id: 4,
+      label: "Title",
+      href: ""
+    },
+  ]
 };
